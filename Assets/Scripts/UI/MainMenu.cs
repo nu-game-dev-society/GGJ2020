ae0c0a2ac783388c87df5b8c6d44ce8e929f268f
rtm516 <ryantmilner@hotmail.co.uk>
2020-01-31 17:26:54 +0000

    Added main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitButton()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
