2eab6967fc1ffeef7c64ece67411ac543345ecad
Joseph Whitehead <44853302+JosephWhi98@users.noreply.github.com>
2020-01-31 21:09:10 +0000

    Audio pool + soundObject code

---
+++ Assets/Scripts/Audio/AudioSourcePooled.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourcePooled : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+
+
+    public void Play(AudioClip clip, float vol = 1f)
+    {
+        audioSource.clip = clip;
+        audioSource.volume = vol;
+        audioSource.Play();
+
+        StartCoroutine(DelayedReturnToPool());
+    }
+
+    IEnumerator DelayedReturnToPool()
+    {
+        yield return new WaitForSeconds(audioSource.clip.length);
+        AudioPool.instance.ReturnToPool(this);
+    }
+}
