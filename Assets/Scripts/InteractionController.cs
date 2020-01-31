498ab4d333f3bd1a9b996c64f27152b7a6fe9dbe
Andrew Nattress <a.nattress@gmail.com>
2020-01-31 22:10:17 +0000

    Interactions added

---
+++ Assets/Scripts/InteractionController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    public PlayerController player;
+    public TextMeshProUGUI pickupText;
+    public InventoryItem currentItem;
+    public Interaction currentInteraction;
+    public GameObject interactionUI;
+    void Start()
+    {
+
+    }
+
+    public void Interact()
+    {
+        if (currentItem)
+        {
+            currentItem.PickedUp(player);
+            player.heldItem = currentItem;
+        }
+        else if (currentInteraction)
+        {
+            currentInteraction.Interact();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "PickupItem":
+                currentItem = other.transform.GetComponent<InventoryItem>();
+                interactionUI.SetActive(true);
+                pickupText.SetText("E to Pickup " + currentItem.itemName);
+                break;
+            case "Interaction":
+                currentInteraction = other.transform.GetComponent<Interaction>();
+                interactionUI.SetActive(true);
+                pickupText.SetText("E to " + currentInteraction.interactionName);
+                break;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "PickupItem":
+                InventoryItem item = other.GetComponent<InventoryItem>();
+                if (item == currentItem)
+                {
+                    if (item.pickupTarget)
+                        return;
+                    currentItem = null;
+                    interactionUI.SetActive(false);
+                }
+                break;
+            case "Interaction":
+                Interaction interaction = other.GetComponent<Interaction>();
+                if (interaction == currentInteraction)
+                {
+                    currentInteraction = null;
+                    interactionUI.SetActive(false);
+                }
+                break;
+        }
+    }
+}
--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryItem
+public class InventoryItem : MonoBehaviour
 {
     public string id;
-    public string name;
+    public string itemName;
     public ItemType type;
-    
+    public PlayerController pickedUpByPlayer;
+    public Transform pickupTarget;
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     public void Use()
     {
         Debug.Log("Used " + name + " (" + id  + ")");
+    }
+    public void PickedUp(PlayerController pc)
+    {
+        pickedUpByPlayer = pc;
+        pickupTarget = pc.handTransform;
+        rb.useGravity = false;
+        Physics.IgnoreCollision(GetComponent<Collider>(), pc.GetComponent<Collider>());
+    }
+    public void Dropped()
+    {
+        pickupTarget = null;
+        rb.useGravity = true;
+    }
+    private void FixedUpdate()
+    {
+        if(pickupTarget)
+        {
+            Vector3 dir = pickupTarget.position - transform.position;
+            float dist = Vector3.Distance(pickupTarget.position, transform.position) * 300f;
+            float fwdBoost = 0;
+            if (pickedUpByPlayer.inputs.v > 0)
+                fwdBoost = pickedUpByPlayer.inputs.v * pickedUpByPlayer.speed * 3f;
+            rb.AddForce(((dir * dist) + (fwdBoost * pickedUpByPlayer.transform.forward)) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
     }
 }
 
