4c62e68960899eb9d1455a650ce4b9d909092322
Patton97 <32955550+Patton97@users.noreply.github.com>
2020-01-31 18:47:27 +0000

    Made prefab folder

---
+++ Assets/Scripts/AI/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    // vars
+    float waitedFor;
+    Transform targetNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: Get an ID from the GameManager?
+        //TODO: Decide how much money they have
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
