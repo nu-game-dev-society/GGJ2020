d8dd6570a149990775c3b5e8ce6a7d92f9b0b84d
rtm516 <ryantmilner@hotmail.co.uk>
2020-02-01 23:01:35 +0000

    Added pause menu

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI moneyText;
+
+    [SerializeField]
+    private GameObject pausePanel;
+
+    private PlayerController playerController;
+    private InputHandler inputHandler;
+
+    private bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+        inputHandler = FindObjectOfType<InputHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = MoneySystem.FormatMoney(MoneySystem.GetMoney());
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        pausePanel.SetActive(isPaused);
+        playerController.enabled = !isPaused;
+        inputHandler.enabled = !isPaused;
+        Time.timeScale = Convert.ToInt32(!isPaused);
+    }
+}
