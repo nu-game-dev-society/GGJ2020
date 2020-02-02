73f2fb21f56d8aaabdac21c41ca689a6b175f2bd
Joseph Whitehead <44853302+JosephWhi98@users.noreply.github.com>
2020-02-02 12:55:31 +0000

    - Scene changes + update

---
+++ Assets/Scripts/TV.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV : MonoBehaviour
+{
+
+
+    public Renderer rend;
+    public Material mat;
+
+    public Material[] images;
+   
+    void Update()
+    {
+        if (Time.frameCount % 400 == 0)
+            rend.sharedMaterial = images[Random.Range(0, 3)];
+    }
+}
