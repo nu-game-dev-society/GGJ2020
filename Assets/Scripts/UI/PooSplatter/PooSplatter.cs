0a2f078ad90efcbcf86645781eb37329a78d4a11
Patton97 <32955550+Patton97@users.noreply.github.com>
2020-02-19 19:23:11 +0000

    Unfinished Poo
    This commit is essentially a GameDev Turtlehead

---
+++ Assets/Scripts/UI/PooSplatter/PooSplatter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooSplatter : MonoBehaviour
+{
+    [SerializeField] List<Sprite> stages;
+    [Range(5,0)]
+    [SerializeField] int stage;
+    float lifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifetime = stage;
+        UpdateStage(stage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+
+        for(int i = 0; i < 5; i++)
+        {
+            if (lifetime < i)
+                UpdateStage(i);
+        }
+        
+    }
+
+    void UpdateStage(int stage)
+    {
+        this.stage = stage;
+    }
+}
---
+++ Assets/Scripts/UI/PooSplatter/PooSplatterManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooSplatterManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> splatterPrefabs;
+    float pooLevel = 0f;
+    List<PooSplatter> activeSplatters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(splatterPrefabs == null)
+        {
+            splatterPrefabs = new List<GameObject>();
+            Debug.LogWarning("WARNING: Nobody serialised the fucking poo splatters");
+        }
+
+        activeSplatters = new List<PooSplatter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pooLevel > 0.0f) DecreasePooLevel(Time.deltaTime);
+        if (pooLevel < 0.0f) ClearPooLevel();
+        if (pooLevel > 1.0f) GenerateSplatter();
+    }
+
+    void GenerateSplatter()
+    {
+        //Generating a new splatter "spends" pooLevel
+        DecreasePooLevel(1.0f);
+
+        //Instantiate a new poo splatter, position it randomly on the screen
+        GameObject go = Instantiate(splatterPrefabs[0], transform);
+        go.transform.position += RandomVector(Screen.width/2,Screen.height/2);
+    }
+
+    Vector3 RandomVector(float xRange = 0f, float yRange = 0f, float zRange = 0f)
+    {
+        return new Vector3()
+        {
+            x = Random.Range(-xRange, xRange),
+            y = Random.Range(-yRange, yRange),
+            z = Random.Range(-zRange, zRange)
+        };
+    }
+
+    void IncreasePooLevel(float amount = 1.0f) => pooLevel += amount;
+    void DecreasePooLevel(float amount = 1.0f) => pooLevel -= amount;
+    void ClearPooLevel() => pooLevel = 0.0f;
+}
