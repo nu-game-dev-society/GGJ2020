d972998642d120761ce2e3d744419af3c63066ca
rtm516 <ryantmilner@hotmail.co.uk>
2020-03-04 19:45:41 +0000

    Added auto MainMenu load from multiplayer scene

---
+++ Assets/Scripts/Editor/MultiplayerRedirect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public static class MultiplayerRedirect
+{
+    static MultiplayerRedirect()
+    {
+        EditorSceneManager.sceneOpened += SceneOpenedCallback;
+    }
+
+    private static void SceneOpenedCallback(Scene scene, OpenSceneMode mode)
+    {
+        SceneAsset playScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(scene.path);
+        if (scene.name == "MultiplayerScene")
+        {
+            playScene = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Scenes/MainMenu.unity");
+        }
+
+        EditorSceneManager.playModeStartScene = playScene;
+    }
+}
