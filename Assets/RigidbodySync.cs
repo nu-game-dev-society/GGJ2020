fd10a7b4fe0be8f1ea532ac172e28f6e8844edd3
Andrew Nattress <a.nattress@gmail.com>
2020-03-04 20:20:10 +0000

    Rigidbody Sync started

---
+++ Assets/RigidbodySync.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(PhotonView))]
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodySync : MonoBehaviour, IPunObservable
+{
+    PhotonView pV;
+    Rigidbody rb;
+    Vector3 networkPosition;
+    Quaternion networkRotation;
+    bool moved = true;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pV = GetComponent<PhotonView>();
+        rb = GetComponent<Rigidbody>();
+        networkPosition = new Vector3();
+        networkRotation = new Quaternion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pV.IsMine)
+        {
+            if (rb.velocity.sqrMagnitude > 0.01f)
+                moved = false;
+        }
+    }
+    public void FixedUpdate()
+    {
+        if (!pV.IsMine)
+        {
+            //rb.position = Vector3.MoveTowards(rb.position, networkPosition, this.m_Distance * (1.0f / PhotonNetwork.SerializationRate));
+            //rb.rotation = Quaternion.RotateTowards(rb.rotation, networkRotation, this.m_Angle * (1.0f / PhotonNetwork.SerializationRate));
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(moved);
+            if (moved)
+            {
+                stream.SendNext(this.rb.position);
+                stream.SendNext(this.rb.rotation);
+
+
+                stream.SendNext(this.rb.velocity);
+
+
+            }
+            moved = false;
+        }
+
+
+
+        else
+        {
+            moved = (bool)stream.ReceiveNext();
+        }
+
+    }
+}
--- Assets/Scripts/DrinkSpawner.cs
+++ Assets/Scripts/DrinkSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +10,7 @@ public class DrinkSpawner : MonoBehaviour
     public class SpawnPoint
     {
         public Transform transform;
-        public GameObject drink; 
+        public GameObject drink;
     }
 
     [System.Serializable]
@@ -19,17 +20,19 @@ public class DrinkSpawner : MonoBehaviour
         public string id;
         public GameObject obj;
         public SpawnPoint[] spawnPoints;
-        public int itemPos; 
-        public ShopItemUI shopItem; 
+        public int itemPos;
+        public ShopItemUI shopItem;
     }
 
-  
+
     public DrinkItem[] drinks;
 
     public Shop ui;
 
+    //public static List<int> poolOfIDs = new List<int>{150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187, 188, 189, 190,191,192,193,194,195,196,197,198,199, 200};
     private void Start()
     {
+        
         foreach (DrinkItem d in drinks)
         {
             if (ui.items[d.itemPos].ownedNum > 0)
@@ -40,7 +43,12 @@ public class DrinkSpawner : MonoBehaviour
                     {
                         if (ui.items[d.itemPos].ownedNum > 0)
                         {
-                            Drink drink = Instantiate(d.obj, p.transform).GetComponent<Drink>();
+                            Drink drink = Instantiate(d.obj, p.transform).GetComponent<Drink>(); //PHOTON VIEW 150 - 200 IS RESERVED FOR DRINKS
+                            if (PhotonNetwork.IsConnected)
+                            {
+                                int x = Random.Range(0, 50);
+                                drink.GetComponent<PhotonView>().ViewID = 150 + x;
+                            }
                             drink.transform.position = p.transform.position;
                             drink.transform.eulerAngles = p.transform.eulerAngles;
                             drink.spawn = p;
@@ -55,7 +63,7 @@ public class DrinkSpawner : MonoBehaviour
 
     private void Update()
     {
-        if (Time.frameCount % 60 == 0 )
+        if (Time.frameCount % 60 == 0)
         {
             foreach (DrinkItem d in drinks)
             {
@@ -81,4 +89,5 @@ public class DrinkSpawner : MonoBehaviour
             }
         }
     }
+
 }
